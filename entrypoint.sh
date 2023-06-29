c68c2e4528e5c7406a8fc3ea941558b2dd0f7f9f
bagred789 <90314458+bagred789@users.noreply.github.com>
2023-06-29 08:21:59 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 ﻿#!/usr/bin/env bash
 
-UUID=${UUID:-'a3e79202-52bd-4e51-ab10-8f2eacb7756c'}
-VMESS_WSPATH=${VMESS_WSPATH:-'/a3e79202-52bd-4e51-ab10-8f2eacb7756c-vmess'}
-VLESS_WSPATH=${VLESS_WSPATH:-'/a3e79202-52bd-4e51-ab10-8f2eacb7756c-vless'}
-TROJAN_WSPATH=${TROJAN_WSPATH:-'/a3e79202-52bd-4e51-ab10-8f2eacb7756c-trojan'}
-SS_WSPATH=${SS_WSPATH:-'/a3e79202-52bd-4e51-ab10-8f2eacb7756c-shadowsocks'}
+UUID=${UUID:-'f01e2550-51f2-47f8-b357-a6f1335edc6e'}
+VMESS_WSPATH=${VMESS_WSPATH:-'/f01e2550-51f2-47f8-b357-a6f1335edc6e-vmess'}
+VLESS_WSPATH=${VLESS_WSPATH:-'/f01e2550-51f2-47f8-b357-a6f1335edc6e-vless'}
+TROJAN_WSPATH=${TROJAN_WSPATH:-'/f01e2550-51f2-47f8-b357-a6f1335edc6e-trojan'}
+SS_WSPATH=${SS_WSPATH:-'/f01e2550-51f2-47f8-b357-a6f1335edc6e-shadowsocks'}
 
 generate_config() {
   cat > config.json << EOF
